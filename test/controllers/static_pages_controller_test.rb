93dd280361c784ae348bcf4889758009ad963901
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-30 08:02:08 +0900

    4.4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,13 +22,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
     # assert_select "title", "Help | #{@base_title}"
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
     # assert_select "title", "About | #{@base_title}"
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
