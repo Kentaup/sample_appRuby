10dd0f52cecedd2c395159f5986001ac573082e8
Kentaup <kenta.mori.scriptg@gmail.com>
2019-10-30 15:01:21 +0900

    6.19n

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -37,8 +37,9 @@ class UserTest < ActiveSupport::TestCase
      @user.email = valid_address
      assert @user.valid?, "#{valid_address.inspect} should be valid"
    end
+ end
 
-   test "email validation should reject invalid addresses" do
+ test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -46,5 +47,4 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-
 end
